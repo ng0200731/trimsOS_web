140485d988806d8bc6dbcb3afabc5a9a82ac3eca
eric <eric.brilliant@gmail.com>
2026-06-26 12:59:58 +0800

    add wrangler config for Cloudflare static deploy

--- web/public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Static-export entry point for PHP-only hosts (e.g. InfinityFree).
-// Apache may prefer index.php over index.html; just serve the prerendered HTML.
-readfile(__DIR__ . '/index.html');
